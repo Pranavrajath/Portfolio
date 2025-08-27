b80a05565e341bbecb09059557ffff7059a46505
pranavrajath <pranavrajath0@gmail.com>
2025-08-27 16:34:38 +0530

    Updated project page

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,4 +1,5 @@
 import { Description } from "@radix-ui/react-toast";
+import { ExternalLink, Github, Key } from "lucide-react";
 
 const projects =[
     {
@@ -8,7 +9,7 @@ const projects =[
         image:"/Projects/buildboxpic.png",
         tags:["React", "TailWindCSS", "MongoDB"],
         demoUrl: "#",
-        githubUrl: "#https://github.com/Pranavrajath/mini-project"
+        githubUrl: "https://github.com/Pranavrajath/mini-project"
     },
     {
         id:2,
@@ -17,14 +18,52 @@ const projects =[
         image:"/Projects/portfoliopic.png",
         tags:["React", "TailWindCSS", "JavaScript"],
         demoUrl: "#",
-        githubUrl: "#https://github.com/Pranavrajath/Portfolio"
+        githubUrl: "https://github.com/Pranavrajath/Portfolio"
     },
 ]
 
 
 export const ProjectSection = () => {
 
-    return <section>
+    return <section id="projects" className="py-25 px-4 relative ">
+        <div className="container mx-auto max-w-5xl">
+            <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center"> 
+            Featured<span className="text-primary"> Projects</span>
+            </h2>
+            <p className="text-xl p-10">
+                At the moment, I've completed only a few projects 
+                - Some are still under construction. Check back later for more!
+            </p>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-10 ">
+                {projects.map((project,key) => (
+                    <div key={key} className=" group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                        <div className="h-48 overflow-hidden">
+                            <img src={project.image} alt={project.title} 
+                                className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                            />
+                        </div>
+                        <div className="p-6">
+                            <div className="flex flex-wrap gap-2 md-4">
+                                {project.tags.map((tag) => (
+                                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-primary/20 text-secondary-foreground"> {tag} </span>
 
+                                ))}
+                            </div>
+                        </div>
+                        <h3 className="text-l font-semibold mb-2">{project.title}</h3>
+                        <p className="text-muted-foreground text-sm pb-7">
+                                {project.Description}
+                        </p>
+                        <div className="flex justify-between items.center">
+                            <div className="flex space-x-3 pb-2 pl-2">
+                                <a href= {project.githubUrl} className="text-foreground/80 hover:text-primary transition=colors duration-300">
+                                    <Github/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     </section> ;
 }
