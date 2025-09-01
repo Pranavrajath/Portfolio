1ef525e522b5b45dae444d23cdab43fe6d392170
pranavrajath <pranavrajath0@gmail.com>
2025-09-01 20:30:44 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss  from '@tailwindcss/vite'
 import path from "path"
-import { IconName } from '@phosphor-icons/react';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: './Portfolio/',
+  base: '/Portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname,"./src"),
