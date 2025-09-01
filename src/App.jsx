353f0d6d5dae19f421d7dd9393a644ee8104814b
pranavrajath <pranavrajath0@gmail.com>
2025-09-01 20:44:30 +0530

    vite config updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Toaster/>
       <BrowserRouter>
       <Routes>
-        <Route index element={<Home/>}/>
+        <Route path="/" index element={<Home/>}/>
         <Route path="*" element={<NotFound/>} />
       </Routes>
       </BrowserRouter>
