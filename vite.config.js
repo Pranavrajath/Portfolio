f5f4cab16ce708c8b4e351e2e1a1a0e0e8675533
pranavrajath <pranavrajath0@gmail.com>
2025-09-01 19:26:10 +0530

    phosphor icons updated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss  from '@tailwindcss/vite'
 import path from "path"
+import { IconName } from '@phosphor-icons/react';
 
 // https://vite.dev/config/
 export default defineConfig({
