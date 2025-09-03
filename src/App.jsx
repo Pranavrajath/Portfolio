b0e095912c60a45165662ceb7f8fed4fdb921323
pranavrajath <pranavrajath0@gmail.com>
2025-09-03 20:25:22 +0530

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import { Home } from "./pages/Home";
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "./components/ui/toaster";
 import { Toast } from "./components/ui/toast";
 
 function App() {
-
   return (
     <>
-    <Toaster/>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" index element={<Home/>}/>
-        <Route path="*" element={<NotFound/>} />
-      </Routes>
-      </BrowserRouter>
+      <Toaster />
+      <HashRouter>
+        <Routes>
+          <Route path="/" index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </HashRouter>
     </>
   );
 }
